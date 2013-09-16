73a30f27a4ec2c65de8a9b70cdc5deb7e2a5676d
rodrigo <rodrigo@corp.sonic.net>
2013-09-16 13:00:51 -0700

    allow plugins to be loaded before session stuff

--- program/include/rcmail.php
+++ program/include/rcmail.php
@@ -86,6 +86,11 @@ class rcmail extends rcube
   {
     $this->init(self::INIT_WITH_DB | self::INIT_WITH_PLUGINS);
 
+    $this->plugins->init();
+
+    // load plugins
+    $this->plugins->load_plugins((array)$this->config->get('plugins', array()), array('filesystem_attachments', 'jqueryui'));
+
     // start session
     $this->session_init();
 
@@ -113,9 +118,7 @@ class rcmail extends rcube
     else
       $GLOBALS['OUTPUT'] = $this->load_gui(!empty($_REQUEST['_framed']));
 
-    // load plugins
-    $this->plugins->init($this, $this->task);
-    $this->plugins->load_plugins((array)$this->config->get('plugins', array()), array('filesystem_attachments', 'jqueryui'));
+    $this->plugins->init_plugins();
   }
 
 
